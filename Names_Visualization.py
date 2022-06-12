12292be6910bcb218713e40978688be969933f96
AdamDataScience <82123890+AdamDataScience@users.noreply.github.com>
2022-06-13 00:16:28 +0200

    Update Names_Visualization.py

--- Names_Visualization.py
+++ Names_Visualization.py
@@ -79,6 +79,7 @@ def plot_name(name, handle_sex='SEPARATE'):
 st.header('French names by year')
     
 name_selected = st.selectbox('Type a name :', unique_names, first_name_index)
+st.experimental_set_query_params(name=name_selected)
 plot_name(name_selected, 'SEPARATE')
 
 st.markdown('INSEE 2021, _Fichier des prénoms_')
