a99c24874a504d7e7849873b046e1ad66707c8ca
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-14 01:09:59 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -35,8 +35,9 @@ def load_data(default_name='CAMILLE', first_name=first_name, remove_rare=True, r
     if remove_X:
         df = df[df.year != "XXXX"]
         df = df[df.dpt != "XX"]
-        df = df.astype({'year':'int32'}).sort_values(by='year')
+        df = df.astype({'year':'int32'})
     unique_names = df.name.unique()
+    df = df.sort_values(by='year')
     if first_name not in unique_names: first_name = default_name
     first_name_index = int(np.where(unique_names == first_name)[0][0])
     return df, unique_names, first_name_index
