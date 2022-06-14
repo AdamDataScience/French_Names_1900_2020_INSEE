6e608db3e4dc08c9f110ba41cf3702bd6e7e5fc1
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-14 18:30:54 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -155,7 +155,7 @@ new_index = new_index.str.zfill(2)
 map_name_data = map_name_data.set_index(['dpt']).reindex(new_index,fill_value=0).reset_index()
 # merge with dpt population:
 map_name_data = pd.merge(map_name_data.astype({'dpt':int}), dpt_pop.astype({'dpt':int}), how='left', on='dpt', suffixes=None)
-map_name_data['prop'] = map_name_data['count'].astype(float) / map_name_data.pop.values.astype(float)
+map_name_data['prop'] = map_name_data['count'].astype(float) / map_name_data['pop'].astype(float)
 with cols[1]:
      st.write(map_name_data)
     
@@ -165,8 +165,8 @@ geojson = load_map_data()
 for idx in range(len(geojson['features'])): # 95
      geojson['features'][idx]['properties']['name'] = name_selected.title()
      geojson['features'][idx]['properties']['count'] = int(map_name_data['count'][idx])
-     geojson['features'][idx]['properties']['population'] = int(map_name_data.pop[idx])
-     geojson['features'][idx]['properties']['proportion'] = float(map_name_data.prop[idx])
+     geojson['features'][idx]['properties']['population'] = int(map_name_data['pop'][idx])
+     geojson['features'][idx]['properties']['proportion'] = float(map_name_data.['prop'][idx])
     
 
 # # find arbitrary country/city's coordinates:
