65be324435a5930ec1d0c67497ca8181ee5eb5b0
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-14 16:53:54 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -10,7 +10,7 @@ from streamlit_folium import folium_static
 import zipfile
 
 import matplotlib as mpl
-dpi = 108 # average dpi
+dpi = 81 # average dpi
 ratio = 1.2
 width = 550
 height = width/ratio
