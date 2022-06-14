1566c99d20b8d9ca2ba97e663602493507a04138
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-14 19:02:18 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -157,7 +157,7 @@ map_name_data = map_name_data.set_index(['dpt']).reindex(new_index,fill_value=0)
 map_name_data = pd.merge(map_name_data.astype({'dpt':int}), dpt_pop.astype({'dpt':int}), how='inner', on='dpt')
 # get percentage of pop
 map_name_data['prop'] = ((map_name_data['count'].astype(float) / map_name_data['pop'].astype(float)) *100) # .astype(int)
-# with cols[1]: st.write(map_name_data)
+with cols[1]: st.write(map_name_data)
     
 geojson = load_map_data()
 # add count to geojson data:
