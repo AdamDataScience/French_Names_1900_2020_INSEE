90c04fc4de08910b6b5bdac658a01fd55c7cfc61
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-22 01:01:44 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -292,11 +292,10 @@ if gen_button:
      pass
 #      st.write(name_data.head(gen_number))
 
-gob = GridOptionsBuilder.from_dataframe(name_data.head(search_number),fit_columns_on_grid_load=True)
+gob = GridOptionsBuilder.from_dataframe(name_data.head(search_number))
 # gob.configure_column('name', editable=True)
 gob.configure_selection('single')
-grid_options = gob.build()
-grid = AgGrid(name_data.head(search_number), grid_options)
+grid = AgGrid(name_data.head(search_number), grid_options=gob.build(), editable=False,fit_columns_on_grid_load=True)
 grid_selected_row = grid['selected_rows']
 st.write(grid_selected_row)
 # st.write(name_data.iloc[grid_selected_row,:])
