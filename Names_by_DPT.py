b5df78d71fb21757846760c77e00c22183c21894
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-06-22 00:32:06 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -292,6 +292,9 @@ if gen_button:
 #      pass
 #      st.write(name_data.head(gen_number))
      grid = AgGrid(name_data.head(search_number), editable=False)
+     grid_selected_row = grid['selected_rows']
+     st.write(grid_selected_row)
+     st.write(name_data.iloc([grid_selected_row,:]))
 
      
      
