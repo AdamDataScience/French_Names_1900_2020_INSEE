9095aa7a4f5c140bc7e3507a84438f210ed5c8a3
adamdatascience <82123890+adamdatascience@users.noreply.github.com>
2022-07-27 14:05:27 +0200

    Update Names_by_DPT.py

--- Names_by_DPT.py
+++ Names_by_DPT.py
@@ -266,7 +266,7 @@ with st.form('search_form'):
 #           search_button = st.button('Search Names', key='search_button', disabled=True)
           search_button = st.form_submit_button('Search Names') # ,key='search_button',disabled=False)
 if search_button:
-     st.mardown('Name search coming soon !')
+     st.markdown('Name search coming soon !')
      # st.write(name_data.head(search_number))
 #      grid = AgGrid(name_data.head(search_number), editable=True)
 
@@ -291,7 +291,7 @@ with st.form('gen_form'):
 #          st.button('Generate Names', disabled=True)
           gen_button = st.form_submit_button('Generate Names') # ,key='gen_button',disabled=False)
 if gen_button:
-     st.mardown('Name generation coming soon !') 
+     st.markdown('Name generation coming soon !') 
      # pass
      # st.write(name_data.head(gen_number))
 
